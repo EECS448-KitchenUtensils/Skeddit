cb91ece1b30afaa2069260f3485e63a274c1bc1d
Zane Cersovsky <zcersovsky@gmail.com>
2017-10-04 21:10:56 -0500

    Added a create implementation to task controller. Removed unnecessary methods from same controller.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,11 +3,11 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
   before_action :load_event
   before_action :owns_event
-  skip_before_action :owns_event, only: [:index, :show]
-  def new
-    @task = Task.new
-  end
-  def index
-    @tasks = Task.all
+  def create
+    @task = Task.new(params[:task].permit(:name).merge(:event => @event, :owner => nil))
+    unless @task.save
+      flash[:error] = "Unable to save task"  
+    end
+    redirect_to @event
   end
 end
