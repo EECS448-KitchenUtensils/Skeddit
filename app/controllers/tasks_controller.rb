002235cd37d13bb15a467d4999cf861237262ad5
Zane Cersovsky <zcersovsky@gmail.com>
2017-10-04 22:12:05 -0500

    Added a destroy method for tasks; made Task.owner optional

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,10 +3,26 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
   before_action :load_event
   before_action :owns_event
+  
+  # Creates a new task in the database
+  # PRE: None
+  # POST: Either a task is created in the database, or an error is returned to the user
   def create
-    @task = Task.new(params[:task].permit(:name).merge(:event => @event, :owner => nil))
+    @task = Task.new(params[:task].permit(:name).merge(:event => @event))
     unless @task.save
       flash[:error] = "Unable to save task"  
+    end
+    redirect_to @event
+  end
+
+  # Deletes a task from the database
+  # PRE: The given task must exist
+  # POST: Either the task is deleted if it existed, or an error is returned to the user
+  def destroy
+    task_id = params[:id]
+    @task = @event.tasks.where(id: task_id).first
+    unless @task.destroy
+      flash[:error] = "Unable to destroy task"
     end
     redirect_to @event
   end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
     belongs_to :event
-    belongs_to :owner, class_name: :User
+    belongs_to :owner, class_name: :User, optional: true
 end
